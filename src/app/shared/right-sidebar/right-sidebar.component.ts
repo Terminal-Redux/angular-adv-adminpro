39d3cdfcd3b0b444019fd46d186646c366661202
Amilcar Gonzalez <amilkargm@gmail.com>
2022-11-24 08:44:15 -0600

    Fin seccion 3 - DiseÃÃño

---
+++ src/app/shared/right-sidebar/right-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-sidebar',
+  templateUrl: './right-sidebar.component.html',
+  styles: [
+  ]
+})
+export class RightSidebarComponent {
+
+}
