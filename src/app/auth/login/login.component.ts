2c0c00a8638c9bfecdcc47e4f9628314ead900a9
Amilcar Gonzalez <amilkargm@gmail.com>
2022-12-14 21:45:20 -0600

    Login w/ & wo/ Google

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+declare const google: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: [
-    './login.component.css'
-  ]
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  @ViewChild('googleBtn') googleBtn?: ElementRef;
 
-  constructor(private router: Router) {}
+  public formSubmitted = false;
+
+  public loginForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService
+  ) {
+    this.loginForm = this.fb.group({
+      email: [
+        localStorage.getItem('email') ?? '',
+        [Validators.required, Validators.email],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      remember: [false],
+    });
+  }
+  ngAfterViewInit(): void {
+    this.googleInit();
+  }
+
+  googleInit() {
+    google.accounts.id.initialize({
+      client_id:
+        '1049241747875-63f3vi8jcflf2b7u5iurf69j1c1ff85t.apps.googleusercontent.com',
+      callback: (response: any) => this.handleCredentialResponse(response),
+    });
+    google.accounts.id.renderButton(
+      // document.getElementById('buttonDiv'),
+      this.googleBtn?.nativeElement,
+      { theme: 'outline', size: 'large' } // customization attributes
+    );
+  }
+
+  handleCredentialResponse(response: any) {
+    console.log('Encoded JWT ID token: ' + response.credential);
+    this.usuarioService.loginGoogle(response.credential).subscribe((resp) => {
+      // Navegar al dashboard
+      this.router.navigateByUrl('/');
+    });
+  }
 
   login() {
-    this.router.navigateByUrl('/');
+    this.usuarioService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        if (this.loginForm.get('remember')?.value) {
+          localStorage.setItem('email', this.loginForm.get('email')?.value);
+        } else {
+          localStorage.removeItem('email');
+        }
+        // Navegar al dashboard
+        this.router.navigateByUrl('/');
+      },
+      (err) => {
+        // Si sucede un error
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
+
+    //console.log(this.loginForm.value);
+    //this.router.navigateByUrl('/');
   }
 }
